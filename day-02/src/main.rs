7496ca75ca6021123c9057a6b31bdb5436acf704
KHTangent <contact@khtangent.com>
2025-12-02 07:50:49 +0100

    Add day 2 part 1 solution

---
+++ day-02/src/main.rs
@@ -0,0 +1,77 @@
+use std::fs;
+
+fn solution(input: &String) -> i64 {
+	let mut total = 0;
+	for range in input.trim().split(",") {
+		let (range_start, range_end) = range
+			.split_once("-")
+			.and_then(|(start, end)| {
+				println!("Parsing {}-{}", start, end);
+				Some((start.parse::<i64>().unwrap(), end.parse::<i64>().unwrap()))
+			})
+			.unwrap();
+		for number in range_start..(range_end + 1) {
+			let number_string = number.to_string();
+			let number_len = number_string.len();
+			if number_string.starts_with("0") {
+				total += number;
+				continue;
+			}
+			if number_len % 2 == 1 {
+				continue;
+			}
+			let (first_half, second_half) = number_string.split_at(number_len / 2);
+			if first_half == second_half {
+				total += number;
+			}
+		}
+	}
+	total
+}
+
+fn solution2(input: &String) -> i64 {
+	let mut sum = 0;
+	for line in input.lines() {
+		let num = line.parse::<i64>().unwrap_or(0);
+		sum += num;
+	}
+	sum
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_solution() {
+		let input = String::from(
+			r"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+		);
+		let answer = solution(&input);
+		assert_eq!(answer, 1227775554);
+	}
+
+	#[test]
+	#[ignore]
+	fn test_solution2() {
+		let input = String::from(
+			r"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+		);
+		let answer = solution2(&input);
+		assert_eq!(answer, 6);
+	}
+}
+
+fn get_entire_input_file() -> String {
+	let filename = "input.txt";
+	let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+	contents
+}
+
+fn main() {
+	let file = get_entire_input_file();
+	let answer = solution(&file);
+	println!("Answer task 1: {}", answer);
+	let answer = solution2(&file);
+	println!("Answer task 2: {}", answer);
+}
