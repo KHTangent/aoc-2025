91951aad639ee474e4e2df6a01b13eeb22746e86
KHTangent <contact@khtangent.com>
2025-12-06 11:57:48 +0100

    Finish day 6

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -22,7 +22,7 @@ fn solution(input: &str) -> i64 {
 	let mut inputs: Vec<Vec<i64>> = vec![];
 	let mut operands: Vec<Operands> = vec![];
 	for line in input.lines() {
-		let is_number_line = line.chars().next().unwrap().is_digit(10);
+		let is_number_line = line.trim_start().chars().next().unwrap().is_digit(10);
 		if is_number_line {
 			inputs.push(
 				line.split_whitespace()
@@ -56,9 +56,45 @@ fn solution(input: &str) -> i64 {
 
 fn solution2(input: &str) -> i64 {
 	let mut sum = 0;
+	let mut numbers_lines_raw: Vec<Vec<char>> = vec![];
+	let mut operands: Vec<Operands> = vec![];
 	for line in input.lines() {
-		let num = line.parse::<i64>().unwrap_or(0);
-		sum += num;
+		let is_number_line = line.trim_start().chars().next().unwrap().is_digit(10);
+		if is_number_line {
+			numbers_lines_raw.push(line.chars().collect());
+		} else {
+			operands.extend(line.split_whitespace().map(Operands::from_str));
+		}
+	}
+	let numbers_length = numbers_lines_raw.len();
+	let mut number_lines_index = numbers_lines_raw[0].len();
+	for operand in operands.into_iter().rev() {
+		let mut numbers: Vec<i64> = vec![];
+		loop {
+			number_lines_index -= 1;
+			let combined: String = (0..numbers_length)
+				.map(|j| numbers_lines_raw[j][number_lines_index])
+				.collect();
+			let trimmed = combined.trim();
+			if trimmed.is_empty() {
+				break;
+			} else {
+				numbers.push(trimmed.parse().unwrap());
+			}
+			if number_lines_index == 0 {
+				break;
+			}
+		}
+		match operand {
+			Operands::ADD => {
+				let problem_sum = numbers.into_iter().fold(0, |prev, v| prev + v);
+				sum += problem_sum;
+			}
+			Operands::MULTIPLY => {
+				let problem_product = numbers.into_iter().fold(1, |prev, v| prev * v);
+				sum += problem_product;
+			}
+		}
 	}
 	sum
 }
@@ -68,8 +104,8 @@ mod tests {
 	use super::*;
 
 	const TEST_INPUT: &str = r"123 328  51 64 
-45 64  387 23 
-6 98  215 314
+ 45 64  387 23 
+  6 98  215 314
 *   +   *   +  
 ";
 
@@ -80,10 +116,9 @@ mod tests {
 	}
 
 	#[test]
-	#[ignore]
 	fn test_solution2() {
 		let answer = solution2(TEST_INPUT);
-		assert_eq!(answer, 6);
+		assert_eq!(answer, 3263827);
 	}
 }
 
