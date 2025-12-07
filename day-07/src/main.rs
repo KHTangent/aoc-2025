b6a11d386ef688c93c141049b90b15707453c25b
KHTangent <contact@khtangent.com>
2025-12-07 10:45:42 +0100

    Add brute-force of day 7 part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -14,51 +14,83 @@ impl TileTypes {
 			_ => Self::Empty,
 		}
 	}
-	fn parse_map(s: &str) -> Vec<Vec<Self>> {
-		s.lines()
-			.map(|line| line.chars().map(|c| TileTypes::from_char(c)).collect())
-			.collect()
-	}
 }
-fn print_map(map: &Vec<Vec<TileTypes>>) {
-	for line in map.iter() {
-		for c in line.iter() {
-			match c {
-				TileTypes::Empty => print!("."),
-				TileTypes::Beam => print!("|"),
-				TileTypes::Splitter => print!("^"),
+struct TileMap(Vec<Vec<TileTypes>>);
+impl TileMap {
+	fn from_string(s: &str) -> Self {
+		TileMap(
+			s.lines()
+				.map(|line| line.chars().map(|c| TileTypes::from_char(c)).collect())
+				.collect(),
+		)
+	}
+	fn print(&self) {
+		for line in self.0.iter() {
+			for c in line.iter() {
+				match c {
+					TileTypes::Empty => print!("."),
+					TileTypes::Beam => print!("|"),
+					TileTypes::Splitter => print!("^"),
+				}
+			}
+			println!()
+		}
+	}
+	fn simulate_splitted(&mut self) -> i64 {
+		let mut times_splitted = 0;
+		for row in 1..self.0.len() {
+			for col in 0..self.0[row].len() {
+				if self.0[row - 1][col] != TileTypes::Beam {
+					continue;
+				}
+				if self.0[row][col] == TileTypes::Splitter {
+					times_splitted += 1;
+					if self.0[row][col - 1] == TileTypes::Empty {
+						self.0[row][col - 1] = TileTypes::Beam;
+					}
+					if self.0[row][col + 1] == TileTypes::Empty {
+						self.0[row][col + 1] = TileTypes::Beam;
+					}
+				} else {
+					self.0[row][col] = TileTypes::Beam;
+				}
 			}
 		}
-		println!()
+		times_splitted
+	}
+	fn paths_up_from(&self, row: usize, col: usize) -> i64 {
+		if row == 0 {
+			return 1;
+		}
+		let mut paths = 0;
+		if self.0[row - 1][col] == TileTypes::Beam {
+			paths += self.paths_up_from(row - 1, col);
+		}
+		if col != 0 && self.0[row][col - 1] == TileTypes::Splitter {
+			paths += self.paths_up_from(row - 1, col - 1);
+		}
+		if (col + 1) < self.0[0].len() && self.0[row][col + 1] == TileTypes::Splitter {
+			paths += self.paths_up_from(row - 1, col + 1);
+		}
+		paths
 	}
 }
 
 fn solution(input: &str) -> i64 {
-	let mut times_splitted = 0;
-	let mut map = TileTypes::parse_map(input.trim());
-	for row in 1..map.len() {
-		for col in 0..map[row].len() {
-			if map[row - 1][col] != TileTypes::Beam {
-				continue;
-			}
-			if map[row][col] == TileTypes::Splitter {
-				times_splitted += 1;
-				if map[row][col - 1] == TileTypes::Empty {
-					map[row][col - 1] = TileTypes::Beam;
-				}
-				if map[row][col + 1] == TileTypes::Empty {
-					map[row][col + 1] = TileTypes::Beam;
-				}
-			} else {
-				map[row][col] = TileTypes::Beam;
-			}
-		}
-	}
-	times_splitted
+	TileMap::from_string(input.trim()).simulate_splitted()
 }
 
 fn solution2(input: &str) -> i64 {
-	(solution(input) - 1) * 2
+	let mut paths = 0;
+	let mut map = TileMap::from_string(input.trim());
+	map.simulate_splitted();
+	let last_row_index = map.0.len() - 1;
+	for i in 0..map.0[0].len() {
+		if map.0[last_row_index][i] == TileTypes::Beam {
+			paths += map.paths_up_from(last_row_index, i);
+		}
+	}
+	paths
 }
 
 #[cfg(test)]
